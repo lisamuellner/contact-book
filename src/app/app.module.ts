32b98d87725f3a2a76002561935200753da1801a
Lisa <e1526345@student.tuwien.ac.at>
2021-08-25 15:24:38 +0200

    add components for contact form, contact list and detail view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactFormComponent,
+    ContactDetailsComponent
   ],
   imports: [
     BrowserModule,
