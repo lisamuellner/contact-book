02fec89c785bbe07c0ea9c3a1079f8e6c9f5b1c0
Lisa <e1526345@student.tuwien.ac.at>
2021-08-26 20:18:31 +0200

    add unit tests for contact list component

--- src/app/contact-list/contact-list.component.spec.ts
+++ src/app/contact-list/contact-list.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { AppState } from '../store/contact.reducer';
 
 import { ContactListComponent } from './contact-list.component';
 
@@ -6,14 +9,34 @@ describe('ContactListComponent', () => {
   let component: ContactListComponent;
   let fixture: ComponentFixture<ContactListComponent>;
 
+  const initialState: AppState = {
+    contactList: {
+      contacts: [
+        {
+          firstName: 'Max',
+          lastName: 'Mustermann',
+          phone: '0664 1234567',
+          email: 'max.mustermann@gmail.com',
+          address: 'Mustermannstraße 1'
+        },
+        {
+          firstName: 'Donald',
+          lastName: 'Duck',
+          phone: '0699 1234567',
+          email: 'donald.duck@gmail.com',
+          address: 'Entenhausen 10'
+        }
+      ]
+    }
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContactListComponent ]
+      declarations: [ ContactListComponent ],
+      providers: [ provideMockStore({ initialState }) ]
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(ContactListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,4 +45,40 @@ describe('ContactListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show a list of contacts', () => {
+    const rows = fixture.debugElement.queryAll(By.css('tbody tr'));
+    expect(rows.length).toEqual(initialState.contactList.contacts.length);
+    rows.forEach((row, index) => {
+      const [firstName, lastName] = row.queryAll(By.css('td')).map(element => element.nativeElement.innerHTML);
+      expect(firstName).toEqual(initialState.contactList.contacts[index].firstName);
+      expect(lastName).toEqual(initialState.contactList.contacts[index].lastName);
+    });
+  });
+
+  it('should emit contact when table row is clicked', () => {
+    spyOn(component.contactSelected, 'emit');
+
+    const firstRow = fixture.debugElement.queryAll(By.css('tbody tr'))[0];
+    const expectedContact = initialState.contactList.contacts[0];
+
+    firstRow.nativeElement.dispatchEvent(new Event('click'));
+
+    expect(component.contactSelected.emit).toHaveBeenCalledWith(expectedContact);
+  });
+  
+  it('should highlight selected row', () => {
+    spyOn(component.contactSelected, 'emit');
+
+    const firstRow = fixture.debugElement.queryAll(By.css('tbody tr'))[0];
+    expect(firstRow.nativeElement.className).not.toEqual('table-active');
+
+    firstRow.nativeElement.dispatchEvent(new Event('click'));
+
+    fixture.detectChanges();
+
+    expect(component.selectedRow).toEqual(0);
+    expect(firstRow.nativeElement.className).toEqual('table-active');
+  });
+
 });
