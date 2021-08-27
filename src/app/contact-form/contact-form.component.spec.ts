2a5612951a2e234247ef430982fae94b0d15c6da
Lisa <e1526345@student.tuwien.ac.at>
2021-08-27 12:49:18 +0200

    add unit tests for contact form component

--- src/app/contact-form/contact-form.component.spec.ts
+++ src/app/contact-form/contact-form.component.spec.ts
@@ -1,25 +1,57 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { addContact } from '../store/contact.actions';
+import { AppState } from '../store/contact.reducer';
 
 import { ContactFormComponent } from './contact-form.component';
 
 describe('ContactFormComponent', () => {
   let component: ContactFormComponent;
   let fixture: ComponentFixture<ContactFormComponent>;
+  let store: MockStore<AppState>;
+
+  const initialState: AppState = {
+    contactList: {
+      contacts: []
+    }
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContactFormComponent ]
+      declarations: [ ContactFormComponent ],
+      providers: [ provideMockStore({ initialState }) ], 
+      imports: [ ReactiveFormsModule ]
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(ContactFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    
+    spyOn(store, 'dispatch');
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch action to create new contact on submit', () => {
+    const newContact = {
+      firstName: 'Ronald', 
+      lastName: 'McDonald',
+      phone: '0699 9876543',
+      email: 'ronald@mcdonald.com',
+      address: 'McDonald-Straße 12'
+    };
+
+    component.contactForm.setValue(newContact);
+    
+    fixture.detectChanges();
+    component.onSubmit()
+
+    expect(store.dispatch).toHaveBeenCalledWith(addContact(newContact));
   });
 });
