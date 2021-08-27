1ada946c68f9395011d99d27ca69496036d73062
Lisa <e1526345@student.tuwien.ac.at>
2021-08-27 12:48:01 +0200

    add unit tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,69 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { provideMockStore } from '@ngrx/store/testing';
 import { AppComponent } from './app.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { Contact } from './store/contact';
+import { AppState } from './store/contact.reducer';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  const initialState: AppState = {
+    contactList: {
+      contacts: [
+        {
+          firstName: 'Max',
+          lastName: 'Mustermann',
+          phone: '0664 1234567',
+          email: 'max.mustermann@gmail.com',
+          address: 'Mustermannstraße 1'
+        },
+        {
+          firstName: 'Donald',
+          lastName: 'Duck',
+          phone: '0699 1234567',
+          email: 'donald.duck@gmail.com',
+          address: 'Entenhausen 10'
+        }
+      ]
+    }
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, 
+        ContactFormComponent, 
+        ContactDetailsComponent, 
+        ContactListComponent
       ],
+      providers: [ provideMockStore({ initialState })], 
+      imports: [ ReactiveFormsModule ]
     }).compileComponents();
-  });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'contact-book'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('contact-book');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('contact-book app is running!');
+  });
+
+  it('should only show contact details if a contact is selected', () => {
+    const getDetailsComponent = () => fixture.debugElement.query(By.directive(ContactDetailsComponent))
+    expect(getDetailsComponent()).toBeNull();
+    const selectedContact: Contact = {
+      firstName: 'Donald',
+      lastName: 'Duck',
+      phone: '0699 1234567',
+      email: 'donald.duck@gmail.com',
+      address: 'Entenhausen 10'
+    };
+    component.selectedContact = selectedContact;
+
+    fixture.detectChanges(); 
+    expect(getDetailsComponent()).not.toBeNull();
   });
 });
